69f19043680d8d2aaa18de92d2546a223d93fed5
Om Mahajna <omrmahajan9477@gmail.com>
2025-06-09 20:24:27 +0530

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+
+import './App.css'
+import Board from './Components/Board'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+      {/* <h1>Tic-Tac-Toe</h1> */}
+      <Board />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Square.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Square(props) {
+    return (
+        <div className='square'
+        onClick = { props.onClick }
+            style={{
+                border: '1px solid',
+                height: '150px',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }} >
+            <h1>{props.value}</h1>
+        </div>
+    )
+}
